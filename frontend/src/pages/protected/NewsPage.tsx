10ec9131e4031fac7121e133d011f461a5cbd05c
Stephan Lanberg <stephan.lanberg@stellarforce.net>
2024-05-23 02:20:56 +0300

    Small fix

--- frontend/src/pages/protected/NewsPage.tsx
+++ frontend/src/pages/protected/NewsPage.tsx
@@ -26,7 +26,7 @@ const NewsPage: React.FC = () => {
     <>
       <Navbar />
       <div className='flex justify-center'>
-        <div className='bg-skin-boxBackgroundSection w-full max-w-[1900px] md:mt-[10px] rounded-t-[12px] min-h-screen relative md:mx-[10px]'>
+        <div className='bg-skin-boxBackgroundSection w-full h-full max-w-[1900px] md:mt-[10px] rounded-t-[12px] min-h-screen relative md:mx-[10px]'>
           <div className='grid gap-2 max-w-8xl mx-5 grid-cols-1 md:grid-cols-2'>
             {articles.map((article, index) => {
               const isLastArticle = index === articles.length - 1;
