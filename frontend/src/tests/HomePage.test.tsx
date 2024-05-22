7a16ae83edb3a01c0b7a556e2bc0605fe9d4ef5f
Stephan Lanberg <stephan.lanberg@stellarforce.net>
2024-05-22 22:59:21 +0300

    Add first tests

---
+++ frontend/src/tests/HomePage.test.tsx
@@ -0,0 +1,16 @@
+import { expect, test } from "@playwright/test";
+
+test("Simple Playwright test", async ({ page }) => {
+  await page.goto("http://localhost:5173");
+
+  // Wait for the page to fully load
+  await page.waitForLoadState("load");
+
+  // Check if the email input field exists
+  const emailInput = await page.$('input[placeholder="Email"]');
+  expect(emailInput).not.toBeNull();
+
+  // Check if the key input field exists
+  const keyInput = await page.$('input[placeholder="Key"]');
+  expect(keyInput).not.toBeNull();
+});
