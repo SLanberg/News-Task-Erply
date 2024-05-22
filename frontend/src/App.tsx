6cb4062d0668f8d1db735e922b750ffeb56b5677
stephan.lanberg@protonmail.ch <stephan.lanberg@protonmail.ch>
2024-05-22 19:26:54 +0300

    Save

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,22 @@ import './styles/main.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/unprotected/LoginPage';
 import NewsPage from './pages/protected/NewsPage';
+import PrivateRoutes from './components/route/PrivateRoute';
+import UnauthenticatedRoutes from './components/route/UnauthenticatedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/news' element={<NewsPage />} />
+ 
+        <Route element={<UnauthenticatedRoutes />}>
+          <Route path='/' element={<HomePage />} />
+        </Route>
+
+        <Route element={<PrivateRoutes />}>
+          <Route path='/news' element={<NewsPage />} />
+        </Route>
+
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/route/PrivateRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectApiKey } from '../../state/slices/newsApiSlice'; // Import selectApiKey from apiSlice
+
+const PrivateRoutes = () => {
+    const apiKey = useSelector(selectApiKey); // Use selectApiKey from apiSlice to get the apiKey
+
+    // If apiKey is not present, allow only "/" route
+    if (!apiKey) {
+      return <Navigate to="/" />;
+    } else {
+      return <Outlet />;
+    }
+};
+
+export default PrivateRoutes;
